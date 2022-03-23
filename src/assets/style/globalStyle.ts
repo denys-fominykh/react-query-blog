328a3ba88f3666897bfd96be8b100c0717fc9db3
denisfominykh <2028902d>
2022-03-24 00:45:45 +0200

    feat: extend global style

--- src/assets/style/globalStyle.ts
+++ src/assets/style/globalStyle.ts
@@ -1,16 +1,35 @@
 import { createGlobalStyle } from 'styled-components/macro';
 
 export const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-    monospace;
-  }
+	@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+
+	* {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
+
+	body {
+		font-family: 'Poppins', sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+
+	code {
+		font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+		monospace;
+	}
+
+	a {
+		text-decoration: none;
+		color: #000;
+	}
+
+	ul {
+		list-style: none;
+	}
+
+	p {
+		margin: 10px 0;
+	}
 `;
