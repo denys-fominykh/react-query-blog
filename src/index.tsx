53962cbea6135e8448c8a572dcc37ce05fc80687
denisfominykh <2028902d>
2022-03-26 23:37:40 +0200

    feat: connect the react query

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import { App } from 'App';
 import { GlobalStyle } from 'assets/style/globalStyle';
+import { ErrorBoundary } from 'components/ErrorBoundary';
 import reportWebVitals from 'reportWebVitals';
+
+const queryClient = new QueryClient();
 
 render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <GlobalStyle />
+        <App />
+      </QueryClientProvider>
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root'),
 );
