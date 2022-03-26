6d388384f1ead2632e031ab826f7d66319f98364
denisfominykh <2028902d>
2022-03-26 23:38:32 +0200

    feat: connect a routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { NavBar } from 'components/NavBar';
 import { Home } from 'pages/Home';
+import { People } from 'pages/People';
+import { PeopleItem } from 'pages/People/PeopleItem';
 
 export function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <NavBar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/people" element={<People />} />
+          <Route path="/people/:itemId" element={<PeopleItem />} />
+        </Routes>
+      </NavBar>
+    </BrowserRouter>
   );
 }
